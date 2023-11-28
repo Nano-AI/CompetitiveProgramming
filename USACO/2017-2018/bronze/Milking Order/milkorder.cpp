f1fe12989ecb8f2e37c3eead46d8a4137a1aec3b
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2023-11-28 12:08:22 -0800

    lol

--- USACO/2017-2018/bronze/Milking Order/milkorder.cpp
+++ USACO/2017-2018/bronze/Milking Order/milkorder.cpp
@@ -1,5 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int cow_count, soc_count, pos_count;
+vector<int> hiec(0);
+
+int rec(vector<int> b, int c) {
+	if (c == soc_count) {
+		for (int i = 0; i < cow_count; i++) {
+			if (b[i] == 0) {
+				return i;
+			}
+		}
+	}
+
+	int m = INT_MAX;
+
+	int at;
+	for (at = 0; at < cow_count; at++) {
+		if (b[at] == hiec[c]) {
+			break;
+		}
+	}
+
+	
+
+	return m;
+}
 
 int main() {
+	cin >> cow_count >> soc_count >> pos_count;
+	vector<int> b(cow_count);
 
+	vector<vector<int>> p(cow_count);
+
+	for (int i = 0; i < soc_count; i++) 
+		int value = 0;
+		cin << value;
+	}
+
+	for (int i = 0; i < cow_count; i++) {
+		int pos, v;
+		cin >> pos >> v;
+		b[pos] = v; 
+	}
+
+	int v = rec(b, 0); 
+
+	return 0;
 }
