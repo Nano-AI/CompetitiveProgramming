3d447d7871e4d9180e91a057c0b825c4cb2a5487
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2024-03-16 10:15:56 -0700

    A

--- USACO/2021-2022/silver/Spaced Out/main.cpp
+++ USACO/2021-2022/silver/Spaced Out/main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int n;
-int value[1000][1000];
-bool cows[1000][1000];
+int value[1001][1001];
+bool cows[1001][1001];
 
 int main() {
 
@@ -19,38 +19,96 @@ int main() {
     }
   }
 
-  for (int t = 0; t < 1; t++) {
-    // first run to fill the max-values immediately
-    for (int i = 0; i < n - 1; i++) {
-      for (int j = 0; j < n - 1; j++) {
-        int around = 0;
-        pair<int, int> m = { i, j };
+  int m = 0;
 
-        for (int x = 0; x <= 1; x++) {
-          for (int y = 0; y <= 1; y++) {
-            pair<int, int> at = { i + x, y + j };
-            if (cows[at.first][at.second]) continue;
-            cout << at.first << " " << at.second << endl;
-            around += (int) cows[at.first][at.second];
-            if (value[m.first][m.second] 
-                < value[at.first][at.second] || cows[m.first][m.second]) {
-              m = at;
-            }
-          }
-        }
-        if (around < 2) {
-          cows[m.first][m.second] = true;
+  for (int t = 0; t <= 1; t++) {
+    bool top = (bool) t;
+    int t_sum = 0;
+    int top_sum = 0;
+    int bottom_sum = 0;
+    for (int x = 0; x < n; x += 2) {
+      for (int y = 0; y < n; y += 2) {
+        // incase they never alternate
+        top_sum += value[y][x] + value[y][x + 1];
+        bottom_sum += value[y + 1][x] + value[y + 1][x + 1];
+        // however if the top does alternate;
+        if (top) {
+          /*
+            t_sum = 1 1
+                    0 0
+          */
+          t_sum += value[y][x] + value[y][x + 1];
+        } else {
+          /*
+            t_sum = 0 0
+                    1 1
+          */
+          t_sum += value[y + 1][x] + value[y + 1][x + 1];
         }
       }
+      // and the top ones MUST alternate every column, otherwise it's invalid
+      top = !top;
     }
+    // store the max value
+    m = max(t_sum, m);
   }
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      cout << cows[i][j];
+  // horizontal sum
+  for (int t = 0; t <= 1; t++) {
+    bool left = (bool) t;
+    int h_sum = 0;
+    int left_sum = 0;
+    int right_sum = 0;
+    for (int x = 0; x < n; x += 2) {
+      for (int y = 0; y < n; y += 2) {
+        // incase they never alternate
+        left_sum += value[y][x] + value[y + 1][x];
+        right_sum += value[y][x + 1] + value[y + 1][x + 1];
+        // however if the top does alternate;
+        if (left) {
+          /*
+            t_sum = 1 1
+                    0 0
+          */
+          t_sum += value[y][x] + value[y][x + 1];
+        } else {
+          /*
+            t_sum = 0 0
+                    1 1
+          */
+          t_sum += value[y + 1][x] + value[y + 1][x + 1];
+        }
+      }
+      // and the top ones MUST alternate every column, otherwise it's invalid
+      top = !top;
     }
-    cout << endl;
+    // store the max value
+    m = max(t_sum, m);
   }
+
+  cout << m << endl;
+
+  // diagonals
+  for (int d = 0; d <= 1; d++) {
+    bool diag = (bool) d;
+    int t_sum = 0;
+    int h_sum = 0;
+    for (int x = 0; x < n; x += 2) {
+      for (int y = 0; y < n; y += 2) {
+        if (diag) {
+          t_sum += value[y][x] + value[y + 1][x + 1];
+          h_sum += value[y][x + 1] + value[y + 1][x];
+        } else {
+          h_sum += value[y][x] + value[y + 1][x + 1];
+          t_sum += value[y][x + 1] + value[y + 1][y];
+        }
+      }
+      diag = !diag;
+    }
+    m = max({ t_sum, m, h_sum });
+  }
+
+  cout << m << endl;
 
   return 0;
 }
